86464c5d799cdd2420c7866f702bf43f22c96516
Rohan-jfs <59194721+Rohan-jfs@users.noreply.github.com>
2020-01-17 00:58:10 -0800

    Add files via upload

---
+++ d1a-spring-mvc-mvn-copy/src/com/demo/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.demo.model.User;
+
+@Controller
+public class HomeController {
+//returns home view name
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		model.addAttribute("message", "Message from controller");
+		return "home";
+	}
+
+	@RequestMapping(value = "/user", method = RequestMethod.POST)
+	public String user( User user, Model model) {
+		System.out.println("User Page Requested");
+		model.addAttribute("userName", user.getUserName());
+		return "user";
+	}
+}
